5fa1793bff30b0c11345f522dc2180fdf7bb5114
Mario Hamann <mariovhamann@gmail.com>
2021-08-01 01:30:13 +0200

    init: sveltekit + tailwind

---
+++ src/routes/index.svelte
@@ -0,0 +1,37 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+	import Counter from '$lib/Counter.svelte';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<section>
+	<h1 class="uppercase text-lg">
+		Hello People of Earth
+	</h1>
+
+	<h2>
+		try editing <strong>src/routes/index.svelte</strong>
+	</h2>
+
+	<Counter />
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+
+	h1 {
+		width: 100%;
+	}
+</style>
